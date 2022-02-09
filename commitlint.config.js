fd68c4c1717e9bffbcbe5e18c40e54f63dc020d6
lisongyu <lisongyu@bailian.ai>
2022-02-09 13:44:11 +0800

    提交文件

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,25 @@ module.exports = {
   // 采用 cz 自定义的提交规范, > .cz-config.js
   extends: ["cz"],
   rules: {
-    // 自定义规则
+    // type 类型定义，表示 git 提交的type 必须在一下类型范围内
+    // 参数一：表示当前验证的错误级别，参数二：表示在什么情况下验证，参数三：【】泛型内容
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "chore",
+        "revert",
+        "build"
+      ]
+    ],
+    // subject 大小写不做校验
+    "subject-case": [0]
   }
 }
